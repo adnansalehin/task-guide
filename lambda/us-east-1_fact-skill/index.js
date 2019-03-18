b8e8923912e8a3f92d95ed27d0125229b87cc0b2
adnan <adnans@lacunaclientscope.com>
2019-03-18 22:47:21 +0000

    added movie crud functionalities

--- lambda/us-east-1_fact-skill/index.js
+++ lambda/us-east-1_fact-skill/index.js
@@ -1,87 +1,211 @@
 /* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
+/* eslint-disable  no-console */
 
-'use strict';
-const Alexa = require('alexa-sdk');
+const Alexa = require('ask-sdk');
+const dbHelper = require('./helpers/dbHelper');
+const GENERAL_REPROMPT = "What would you like to do?";
+const dynamoDBTableName = "dynamodb-starter";
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hello there. What is your favourite movie? You can say add moviename to add your favourite movie or say list my movies to get your favourite movies.';
+    const repromptText = 'What would you like to do? You can say HELP to get available options';
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = undefined;
-
-const SKILL_NAME = 'Task Guide';
-const GET_GUIDE_MESSAGE = "Here's the list of tasks that I can help you with: ";
-const HELP_MESSAGE = 'You can say help me with a task, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
-
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
-const data = [
-	// 'A year on Mercury is just 88 days long.',
-	'checking your account balance',
-	'washing your clothes',
-	// 'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-	// 'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-	// 'On Mars, the Sun appears about half the size as it does on Earth.',
-	// 'Earth is the only planet not named after a god.',
-	// 'Jupiter has the shortest day of all the planets.',
-	// 'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-	// 'The Sun contains 99.86% of the mass in the Solar System.',
-	// 'The Sun is an almost perfect sphere.',
-	// 'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-	// 'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-	// 'The temperature inside the Sun can reach 15 million degrees Celsius.',
-	// 'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
-
-const handlers = {
-	'LaunchRequest': function () {
-		this.emit('GetTaskList');
-		// this.emit('SpeakTask');
-	},
-	'GetTaskList': function () {
-		
-		this.response.cardRenderer(SKILL_NAME, data[0]);
-		this.response.speak(GET_GUIDE_MESSAGE+data);
-		
-		this.emit(':responseReady');
-	},
-	'AMAZON.HelpIntent': function () {
-		const speechOutput = HELP_MESSAGE;
-		const reprompt = HELP_REPROMPT;
-
-		this.response.speak(speechOutput).listen(reprompt);
-		this.emit(':responseReady');
-	},
-	'AMAZON.CancelIntent': function () {
-		this.response.speak(STOP_MESSAGE);
-		this.emit(':responseReady');
-	},
-	'AMAZON.StopIntent': function () {
-		this.response.speak(STOP_MESSAGE);
-		this.emit(':responseReady');
-	},
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .getResponse();
+  },
 };
 
-exports.handler = function (event, context, callback) {
-	const alexa = Alexa.handler(event, context, callback);
-	alexa.APP_ID = APP_ID;
-	alexa.registerHandlers(handlers);
-	alexa.execute();
+const InProgressAddMovieIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' &&
+      request.intent.name === 'AddMovieIntent' &&
+      request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    const currentIntent = handlerInput.requestEnvelope.request.intent;
+    return handlerInput.responseBuilder
+      .addDelegateDirective(currentIntent)
+      .getResponse();
+  }
+}
+
+const AddMovieIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AddMovieIntent';
+  },
+  async handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const movieName = slots.MovieName.value;
+    return dbHelper.addMovie(movieName, userID)
+      .then((data) => {
+        const speechText = `You have added movie ${movieName}. You can say add to add another one or remove to remove movie`;
+        return responseBuilder
+          .speak(speechText)
+          .reprompt(GENERAL_REPROMPT)
+          .getResponse();
+      })
+      .catch((err) => {
+        console.log("Error occured while saving movie", err);
+        const speechText = "we cannot save your movie right now. Try again!"
+        return responseBuilder
+          .speak(speechText)
+          .getResponse();
+      })
+  },
 };
+
+const GetMoviesIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'GetMoviesIntent';
+  },
+  async handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+    return dbHelper.getMovies(userID)
+      .then((data) => {
+        var speechText = "Your movies are "
+        if (data.length == 0) {
+          speechText = "You do not have any favourite movie yet, add movie by saving add moviename "
+        } else {
+          speechText += data.map(e => e.movieTitle).join(", ")
+        }
+        return responseBuilder
+          .speak(speechText)
+          .reprompt(GENERAL_REPROMPT)
+          .getResponse();
+      })
+      .catch((err) => {
+        const speechText = "we cannot get your movie right now. Try again!"
+        return responseBuilder
+          .speak(speechText)
+          .getResponse();
+      })
+  }
+}
+
+const InProgressRemoveMovieIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' &&
+      request.intent.name === 'RemoveMovieIntent' &&
+      request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    const currentIntent = handlerInput.requestEnvelope.request.intent;
+    return handlerInput.responseBuilder
+      .addDelegateDirective(currentIntent)
+      .getResponse();
+  }
+}
+
+const RemoveMovieIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'RemoveMovieIntent';
+  }, 
+  handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const movieName = slots.MovieName.value;
+    return dbHelper.removeMovie(movieName, userID)
+      .then((data) => {
+        const speechText = `You have removed movie with name ${movieName}, you can add another one by saying add`
+        return responseBuilder
+          .speak(speechText)
+          .reprompt(GENERAL_REPROMPT)
+          .getResponse();
+      })
+      .catch((err) => {
+        const speechText = `You do not have movie with name ${movieName}, you can add it by saying add`
+        return responseBuilder
+          .speak(speechText)
+          .reprompt(GENERAL_REPROMPT)
+          .getResponse();
+      })
+  }
+}
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can introduce yourself by telling me your name';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    InProgressAddMovieIntentHandler,
+    AddMovieIntentHandler,
+    GetMoviesIntentHandler,
+    InProgressRemoveMovieIntentHandler,
+    RemoveMovieIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .withTableName(dynamoDBTableName)
+  .withAutoCreateTable(true)
+  .lambda();
